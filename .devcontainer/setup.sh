1e4ada62bf030f8b586145961a77a14fd9cecdea
SrikRaja <70096426+SrikRaja@users.noreply.github.com>
2026-08-27 15:26:48 +0000

    chore: add devcontainer with docker-outside-of-docker, kubectl, and kind

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/latest/kind-linux-amd64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
+
+kind version
+kubectl version --client
